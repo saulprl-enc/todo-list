d74e7d9715c87062d0532dcd0ba3a982b7040691
Saúl Ramos <saul.ramos@encora.com>
2024-08-01 19:35:23 -0700

    Add todo model, priority enum, basic endpoints, validators

--- todolistbackend/src/main/java/com/todolist/todolistbackend/controllers/TodosController.java
+++ todolistbackend/src/main/java/com/todolist/todolistbackend/controllers/TodosController.java
@@ -1,12 +1,68 @@
 package com.todolist.todolistbackend.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.todolist.todolistbackend.dto.Todo;
+import com.todolist.todolistbackend.enums.TodoPriority;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.*;
 
 @RestController
+@RequestMapping("/todos")
 public class TodosController {
-    @GetMapping("/todos")
-    public String getTodos() {
-        return "You have no todos yet. Start adding some!";
+    private Map<String, Todo> todos = new HashMap<>() {{
+        put("1", new Todo("1", "Finish TodoList", TodoPriority.HIGH));
+    }};
+
+    @GetMapping
+    public Collection<Todo> getTodos() {
+        return this.todos.values();
+    }
+
+    @GetMapping("/{id}")
+    public Todo getTodo(@PathVariable String id) {
+        Todo todo = todos.get(id);
+
+        if (todo == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        return todo;
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody @Valid Todo todo) {
+        todo.setId(UUID.randomUUID().toString());
+        todo.setCreatedAt(new Date());
+        todos.put(todo.getId(), todo);
+
+        return todo;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable String id) {
+        Todo deletedTodo = todos.remove(id);
+
+        if (deletedTodo == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
     }
 }
---
+++ todolistbackend/src/main/java/com/todolist/todolistbackend/dto/Todo.java
@@ -0,0 +1,109 @@
+package com.todolist.todolistbackend.dto;
+
+import com.todolist.todolistbackend.enums.TodoPriority;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+
+public class Todo {
+    private String id;
+
+    @NotEmpty(message = "You must provide a title")
+    @Max(value = 120, message = "The title must not be longer than 120 characters")
+    private String title;
+    private Date due;
+    private boolean done = false;
+    private Date completed;
+
+    @NotNull(message = "You must provide a ToDo priority level")
+    private TodoPriority priority;
+    private Date createdAt;
+
+    public Todo() {
+    }
+
+    public Todo(String id, String title, Date due, boolean done, Date completed, TodoPriority priority, Date createdAt) {
+        this.id = id;
+        this.title = title;
+        this.due = due;
+        this.done = done;
+        this.completed = completed;
+        this.priority = priority;
+        this.createdAt = createdAt;
+    }
+
+    public Todo(String title, TodoPriority priority) {
+        this.title = title;
+        this.priority = priority;
+    }
+
+    public Todo(String title, Date due, TodoPriority priority) {
+        this.title = title;
+        this.due = due;
+        this.priority = priority;
+    }
+
+    public Todo(String id, String title, TodoPriority priority) {
+        this.id = id;
+        this.title = title;
+        this.priority = priority;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDue() {
+        return due;
+    }
+
+    public void setDue(Date due) {
+        this.due = due;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Date getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Date completed) {
+        this.completed = completed;
+    }
+
+    public TodoPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(TodoPriority priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
