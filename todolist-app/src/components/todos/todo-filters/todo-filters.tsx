def9d13c33392711145657643fbc383003808197
Saúl Ramos <saul.ramos@encora.com>
2024-08-08 14:31:12 -0700

    Add priority filter

--- todolist-app/src/components/todos/todo-filters/todo-filters.tsx
+++ todolist-app/src/components/todos/todo-filters/todo-filters.tsx
@@ -13,12 +13,15 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Checkbox } from "@/components/ui/checkbox";
 
 const TodoFiltersForm = z.object({
-  name: z
-    .string()
-    .min(3, { message: "The title must be at least 3 characters long." })
-    .optional(),
+  name: z.string().optional(),
   priority: z.object({
     high: z.boolean().default(true),
     medium: z.boolean().default(true),
@@ -49,6 +52,30 @@ export const TodoFilters = () => {
     },
   });
 
+  const buildPriorityDisplayValue = (value: TodoFiltersForm["priority"]) => {
+    if (!value.high && !value.medium && !value.low) {
+      // form.setValue("priority", { high: true, medium: true, low: true });
+      form.setValue("priority.high", true);
+      form.setValue("priority.medium", true);
+      form.setValue("priority.low", true);
+
+      return;
+    }
+
+    const selectedPriorities: Array<string> = [];
+
+    if (value.high) selectedPriorities.push("High");
+    if (value.medium) selectedPriorities.push("Medium");
+    if (value.low) selectedPriorities.push("Low");
+
+    const displayValue =
+      selectedPriorities.length === 3
+        ? `All, ${selectedPriorities.join(", ")}`
+        : selectedPriorities.join(", ");
+
+    return displayValue;
+  };
+
   const onSubmit = (data: TodoFiltersForm) => {
     console.log(data);
   };
@@ -71,6 +98,71 @@ export const TodoFilters = () => {
                 </FormControl>
               </div>
               <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="priority"
+          render={({ field }) => (
+            <FormItem>
+              <div className="flex items-center gap-2">
+                <FormLabel>Priority</FormLabel>
+                <FormControl>
+                  <Popover>
+                    <PopoverTrigger className="rounded-sm border-2 border-primary p-1">
+                      {buildPriorityDisplayValue(field.value)}
+                    </PopoverTrigger>
+                    <PopoverContent className="flex w-fit flex-col gap-2">
+                      <FormField
+                        control={form.control}
+                        name="priority.high"
+                        render={({ field }) => (
+                          <FormItem className="flex items-center gap-2 space-y-0">
+                            <FormControl>
+                              <Checkbox
+                                checked={field.value}
+                                onCheckedChange={field.onChange}
+                              />
+                            </FormControl>
+                            <FormLabel>High</FormLabel>
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="priority.medium"
+                        render={({ field }) => (
+                          <FormItem className="flex items-center gap-2 space-y-0">
+                            <FormControl>
+                              <Checkbox
+                                checked={field.value}
+                                onCheckedChange={field.onChange}
+                              />
+                            </FormControl>
+                            <FormLabel>Medium</FormLabel>
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="priority.low"
+                        render={({ field }) => (
+                          <FormItem className="flex items-center gap-2 space-y-0">
+                            <FormControl>
+                              <Checkbox
+                                checked={field.value}
+                                onCheckedChange={field.onChange}
+                              />
+                            </FormControl>
+                            <FormLabel>Low</FormLabel>
+                          </FormItem>
+                        )}
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </FormControl>
+              </div>
             </FormItem>
           )}
         />
