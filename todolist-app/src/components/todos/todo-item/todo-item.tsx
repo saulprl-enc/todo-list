84a9a563d082ca725cbfacb0b8a55b9cbc826a5a
Saúl Ramos <saul.ramos@encora.com>
2024-08-10 22:22:14 -0700

    Add item actions, add table header

--- todolist-app/src/components/todos/todo-item/todo-item.tsx
+++ todolist-app/src/components/todos/todo-item/todo-item.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
 import { FC, ReactNode } from "react";
 
@@ -7,17 +9,54 @@ interface Props {
 }
 
 export const TodoItem: FC<Props> = ({ className, children }) => {
-  return <li className={cn(className)}>{children}</li>;
+  return (
+    <li
+      className={cn(
+        "flex items-center gap-2 rounded-sm p-2 transition-colors hover:bg-muted",
+        className,
+      )}
+    >
+      {children}
+    </li>
+  );
+};
+
+export const TodoItemCheck: FC = () => {
+  return <Checkbox />;
 };
 
 export const TodoItemTitle: FC<Props> = ({ className, children }) => {
-  return <span className={cn(className)}>{children}</span>;
+  return (
+    <span className={cn("flex w-2/5 flex-wrap", className)}>{children}</span>
+  );
 };
 
 export const TodoItemPriority: FC<Props> = ({ className, children }) => {
-  return <span className={cn(className)}>{children}</span>;
+  return (
+    <span className={cn("flex w-1/6 flex-wrap capitalize", className)}>
+      {children}
+    </span>
+  );
 };
 
 export const TodoItemDueDate: FC<Props> = ({ className, children }) => {
-  return <span className={cn(className)}>{children}</span>;
+  return <span className={cn("flex w-1/6", className)}>{children}</span>;
+};
+
+export const TodoItemActions: FC<Props> = ({ className, children }) => {
+  return (
+    <div
+      className={cn("flex items-center justify-end gap-1", className)}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const TodoItemEditButton = () => {
+  return <Button variant="secondary">Edit</Button>;
+};
+
+export const TodoItemDeleteButton = () => {
+  return <Button variant="destructive">Delete</Button>;
 };
--- todolist-app/src/components/todos/todo-table/todo-table.tsx
+++ todolist-app/src/components/todos/todo-table/todo-table.tsx
@@ -1,23 +1,46 @@
+import { format } from "date-fns";
+
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useTodos } from "@/context/todos-context";
 import {
   TodoItem,
+  TodoItemActions,
+  TodoItemCheck,
+  TodoItemDeleteButton,
   TodoItemDueDate,
+  TodoItemEditButton,
   TodoItemPriority,
   TodoItemTitle,
 } from "../todo-item/todo-item";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export const TodoTable = () => {
   const { data } = useTodos();
 
   return (
-    <ScrollArea className="h-full w-full rounded-sm border-2 border-primary">
+    <ScrollArea className="relative h-full w-full rounded-sm border-2 border-primary">
+      <div className="sticky left-0 top-0 border-b border-b-slate-400 flex w-full items-center gap-2 bg-secondary p-4 font-bold">
+        <Checkbox />
+        <span className="flex w-2/5">Title</span>
+        <span className="flex w-1/6">Priority</span>
+        <span className="flex w-1/6">Due date</span>
+        <div className="flex flex-grow"></div>
+        <span className="flex w-1/6 justify-end">Actions</span>
+      </div>
       <ul className="flex min-w-full flex-col items-stretch gap-2 p-2">
         {data.map((todo) => (
           <TodoItem key={todo.id}>
+            <TodoItemCheck />
             <TodoItemTitle>{todo.title}</TodoItemTitle>
-            <TodoItemPriority>{todo.priority.toUpperCase()}</TodoItemPriority>
-            <TodoItemDueDate>{todo.due}</TodoItemDueDate>
+            <TodoItemPriority>{todo.priority.toLowerCase()}</TodoItemPriority>
+            <TodoItemDueDate>
+              {todo.due ? format(todo.due, "yyyy/MM/dd") : "-"}
+            </TodoItemDueDate>
+            <div className="flex flex-grow"></div>
+            <TodoItemActions>
+              <TodoItemEditButton />
+              <TodoItemDeleteButton />
+            </TodoItemActions>
           </TodoItem>
         ))}
       </ul>
