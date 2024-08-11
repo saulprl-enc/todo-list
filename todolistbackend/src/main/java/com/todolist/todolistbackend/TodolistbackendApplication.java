da165cc0c4d2ea30c8baec7ea2a3e280369b4efc
Saúl Ramos <saul.ramos@encora.com>
2024-08-10 19:36:15 -0700

    Enable global cors

--- todolistbackend/src/main/java/com/todolist/todolistbackend/TodolistbackendApplication.java
+++ todolistbackend/src/main/java/com/todolist/todolistbackend/TodolistbackendApplication.java
@@ -2,12 +2,25 @@ package com.todolist.todolistbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodolistbackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodolistbackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodolistbackendApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("*");
+            }
+        };
+    }
 
 }
