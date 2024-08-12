8bd70e033a9ab6ae02493407c602d3146281659c
Saúl Ramos <saul.ramos@encora.com>
2024-08-11 17:26:49 -0700

    Apply cors to all endpoints

--- todolistbackend/src/main/java/com/todolist/todolistbackend/TodolistbackendApplication.java
+++ todolistbackend/src/main/java/com/todolist/todolistbackend/TodolistbackendApplication.java
@@ -18,7 +18,7 @@ public class TodolistbackendApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
             }
         };
     }
