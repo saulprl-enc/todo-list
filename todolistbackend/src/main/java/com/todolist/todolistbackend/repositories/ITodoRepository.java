c993464f43ce1e7e22c23a80996564c0ea034db1
Saúl Ramos <saul.ramos@encora.com>
2024-08-13 20:51:20 -0700

    Rename repository interface and implementation

--- todolistbackend/src/main/java/com/todolist/todolistbackend/repositories/ITodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.todolist.todolistbackend.repositories;
-
-import com.todolist.todolistbackend.model.Todo;
-
-import java.util.List;
-
-public interface ITodoRepository {
-    List<Todo> findTodos();
-}
